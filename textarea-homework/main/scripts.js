da91c0fb45e93af22ce429275d08c9a90556fc3d
katkusmt <katkusmt@gmail.com>
2018-11-07 23:02:58 +0200

    event listener added to textarea to expand it when limits are reached

--- textarea-homework/main/scripts.js
+++ textarea-homework/main/scripts.js
@@ -3,12 +3,27 @@ class MinMax {
     this.minRows = min
     this.maxRows = max
     this.mainTextarea = document.getElementById(`main-textarea`)
+    this.rowsMaxChars = 0
   }
 
   setMinAndMaxRows () {
     this.mainTextarea.rows = this.minRows
+    this.rowsMaxChars = this.mainTextarea.rows * this.mainTextarea.cols
+  }
+
+  addInputListenerToTextarea () {
+    let that = this
+    if (this.mainTextarea != null) {
+      this.mainTextarea.addEventListener(`input`, function () {
+        if (that.rowsMaxChars < this.value.length) {
+          this.rows += 1
+          that.rowsMaxChars = this.rows * this.cols
+        }
+      })
+    }
   }
 }
 
 let item = new MinMax(3, 6)
 item.setMinAndMaxRows()
+item.addInputListenerToTextarea()
