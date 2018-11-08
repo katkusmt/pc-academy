bcad16dbf6ab13a2d56df0f9dff76ac897487742
katkusmt <katkusmt@gmail.com>
2018-11-08 23:46:03 +0200

    working on solving scrollbar appearing

--- textarea-homework/main/scripts.js
+++ textarea-homework/main/scripts.js
@@ -1,14 +1,18 @@
-class MinMax {
+class ResizingTextArea {
   constructor (min, max) {
     this.minRows = min
     this.maxRows = max
     this.mainTextarea = document.getElementById(`main-textarea`)
-    this.rowsMaxChars = 0
+    this.rowsMaxChars = this.mainTextarea.rows * this.mainTextarea.cols
+    this.mainTextarea.rows = this.minRows
   }
 
-  setMinAndMaxRows () {
-    this.mainTextarea.rows = this.minRows
-    this.rowsMaxChars = this.mainTextarea.rows * this.mainTextarea.cols
+  addsomeshit () {
+    if (this.mainTextarea.rows === this.minRows) {
+      this.mainTextarea.classList.add(`no-vertical-scrollbar`)
+    } else if (this.rowsMaxChars < this.mainTextarea.value.length && this.mainTextarea.classList.contains(`no-vertical-scrollbar`)) {
+      this.mainTextarea.classList.remove(`no-vertical-scrollbar`)
+    }
   }
 
   hasExceededMaxRows () {
@@ -22,7 +26,7 @@ class MinMax {
       if (that.hasExceededMaxRows()) {
         if (this.rows < that.maxRows) {
           this.rows += 1
-          that.rowsMaxChars = this.rows * this.cols
+          that.rowsMaxChars = this.rows * this.cols + this.rows
         }
       }
     })
@@ -35,14 +39,17 @@ class MinMax {
       if (keyCode === 8 || keyCode === 46) {
         if (this.rows > that.minRows) {
           this.rows -= 1
-          that.rowsMaxChars = this.rows * this.cols
+          that.rowsMaxChars = this.rows * this.cols + this.rows
+          if (this.value.length <= that.rowsMaxChars) {
+            this.rows = that.minRows
+          }
+          that.addsomeshit()
         }
       }
     })
   }
 }
 
-let item = new MinMax(3, 6)
-item.setMinAndMaxRows()
+let item = new ResizingTextArea(3, 6)
 item.addInputListenerToTextarea()
 item.addBackspaceLisenerToTextArea()
